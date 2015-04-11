b553501ad909ab937408bf98685ab5b41050c5f0
Ben Centra <bcentra@constantcontact.com>
2015-04-11 14:41:48 -0400

    adding files

---
+++ Grunfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  // Loads all grunt tasks from package.json
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Variables
+    srcRoot:        'src/',
+    testRoot:       'test/',
+    buildRoot:      'build/',
+    scssPath:       '<%= srcRoot %>style/',
+    scssFile:       '<%= scssPath %><%= pkg.name %>.scss',
+    scssIncludes:   '<%= scssPath %>**/*.scss',
+    jsPath:         '<%= srcRoot %>js/',
+    jsFiles:        '<%= jsPath %>**/*.js',
+    distPath:       '<%= buildRoot %>dist/',
+    distCSSFile:    '<%= distPath %><%= pkg.name %>.css',
+    distCSSMinFile: '<%= distPath %><%= pkg.name %>.min.css',
+    distJSFile:     '<%= distPath %><%= pkg.name %>.js',
+    distJSMinFile:  '<%= distPath %><%= pkg.name %>.min.js',
+    distJSMapFile:  '<%= distPath %><%= pkg.name %>.min.map',
+
+    // Compile Sass to CSS - https://github.com/sindresorhus/grunt-sass
+    sass: {
+      target: {
+        files: {
+          '<%= distCSSFile %>': '<%= scssFile %>' 
+        }
+      }
+    },
+
+    // Minify CSS - https://github.com/gruntjs/grunt-contrib-cssmin
+    cssmin: {
+      options: {
+        banner: '/*! <%= pkg.name %>.min.css, v<%= pkg.version %>, minified <%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      target: {
+        files: {
+          '<%= distCSSMinFile %>': ['<%= distCSSFile %>']
+        }
+      }
+    },
+
+    // Minify JS - https://github.com/gruntjs/grunt-contrib-uglify
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %>.min.js, v<%= pkg.version %>, minified <%= grunt.template.today("yyyy-mm-dd") %> */',
+        sourceMap: true
+      },
+      min: {
+        files: {
+            '<%= distJSMinFile %>': ['<%= jsFiles %>']
+        }
+      }
+    },
+
+    // Run a local web server - https://github.com/gruntjs/grunt-contrib-connect
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          keepalive: true,
+          base: './',
+          hostname: '*',
+          open: {
+            target: 'http://localhost:8000/demo'
+          }
+        }
+      }
+    },
+
+    // Watch for changes in CSS/JS files - https://github.com/gruntjs/grunt-contrib-watch
+    watch: {
+      scss: {
+        files: ['<%= scssFile %>', '<%= scssIncludes %>'],
+        tasks: ['sass', 'cssmin']
+      },
+      scripts: {
+        files: ['<%= jsFiles %>'],
+        tasks: ['jshint', 'uglify']
+      }
+    },
+
+    // Run jshint against JavaScript files - https://github.com/gruntjs/grunt-contrib-jshint
+    jshint: {
+      jshintrc: '.jshintrc',
+      files: ['Gruntfile.js', '<%= jsFiles %>']
+    }
+  });
+
+  grunt.registerTask('build',     ['sass', 'cssmin', 'jshint', 'uglify']);
+  grunt.registerTask('dev',       ['build', 'watch']);
+  grunt.registerTask('serve',     ['connect']);
+  grunt.registerTask('default',   ['dev']);
+
+};
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # grunt-example
 An example client-side project setup with Grunt for running build tasks
+
+## Project Structure
+
+```
+/
+... build/
+... src/
+... test/
+... .jshintrc
+... Gruntfile.js
+... package.json
+```
+## Grunt Tasks
+
+* `grunt` or `grunt dev` - Run all build tasks and watch for changes
+* `grunt build` - Compile Sass, minify and concat CSS and JS files
+* `grunt serve` - Run app in local web server at http://localhost:8000/ 
+
